07005e365d6af61c14ba4a55d4e5d86d2bd32ff6
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2019-01-09 15:43:55 +0700

    Add Todo Submit Handler

--- src/TodoApp/index.js
+++ src/TodoApp/index.js
@@ -3,14 +3,42 @@ import React, { Component } from "react";
 class TodoApp extends Component {
   constructor() {
     super();
+
+    this.setState({
+      input: "",
+      todos: [
+        {
+          id: 0,
+          text: "Learn to make Todo"
+        },
+        {
+          id: 1,
+          text: "Design TodoApp"
+        }
+      ]
+    });
   }
+
+  handleTodoInput = event => {
+    this.setState({ nput: event.target.value });
+  };
+
+  handleTodoSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.input);
+  };
 
   render() {
     return (
       <div>
         <h1>Todo App</h1>
-        <form>
-          <input type="text" placeholder="Anything to do ?" />
+        <form onSubmit={this.handleTodoSubmit}>
+          <input
+            type="text"
+            placeholder="Anything to do ?"
+            onChange={this.handleTodoInput}
+            value={this.state.input}
+          />
           <input type="submit" value="Add to do" />
         </form>
       </div>
